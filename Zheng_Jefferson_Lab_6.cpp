978f06d8006eba870da6001c4f452ff17b3d362e
MythicalPhantom <158225349+MythicalPhantom@users.noreply.github.com>
2024-10-15 15:40:10 -0400

    Update Zheng_Jefferson_Lab_6.cpp

--- Zheng_Jefferson_Lab_6.cpp
+++ Zheng_Jefferson_Lab_6.cpp
@@ -8,21 +8,26 @@
 using namespace std;
 
 int main () {
+    //Variables (Files, Integers, Characters, and Strings)
     ifstream inputFile;
     ofstream outputFile;
     int integer1, integer2;
     char character1;
     string string1;
 
+    //Open Files as Variables
     inputFile.open("inData.txt");
     outputFile.open("outData.txt");
 
-
+    //Takes Values from inData.txt
     inputFile >> integer1 >> integer2 >> character1 >> string1;
-    outputFile << integer1 + integer2 << "\n" << int(character1) + 1 << "\n" << string1;
+    //Outputs Values (Modified) to outData.txt
+    outputFile << integer1 + integer2 << "\n" << char(int(character1) + 1) << "\n" << string1;
 
+    //Closes Files
     inputFile.close();
     outputFile.close();
 
+    //Exits Successfully
     return 0;
 }
